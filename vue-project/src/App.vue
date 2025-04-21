257feb8d0b4f0e984e1d9a450f68206fa11c8f43
MohanManirul <fiforeg@gmail.com>
2025-04-21 22:18:02 +0600

    call api

---
+++ vue-project/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>My Vue App</h1>
+    <p>{{ products }}</p>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script setup >
+import { ref } from "vue";
+
+
+  const products = ref([]) ;
+callApi()
+  async function callApi() {
+    const res = await fetch("https://dummyjson.com/products");
+   
+    const data = await res.json();
+    console.log(data);
+    
+    products.value = data ;
+  }
+</script>
