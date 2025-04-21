79ea69c9281a9f35d2f2112993c2cebd85844cf4
MohanManirul <fiforeg@gmail.com>
2025-04-21 22:58:25 +0600

    for loop vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,8 +1,38 @@
 <template>
-  <div>
+  <div class="container">
     <h1>My Vue App</h1>
-    <p>{{ products }}</p>
+  
+  <div v-if="isLoading">
+      <div >Loading...</div>
+  </div>
+  <div v-else >
+    <table class="table table-striped">
+
+        <thead class="">
+
+          <tr>
+            <th>title</th>
+            <th>category</th>
+            <th>price</th>
+          </tr>
+
+        </thead>
+
+
+
+        <tbody>
+          <tr v-for="item in products" :key="item.id" >
+            <td>{{item.title}}</td>
+            <td>{{item.category}}</td>
+            <td>{{item.price}}</td>
+          </tr>
+        </tbody>
+
+      </table>
+  </div>
+
     <router-view></router-view>
+
   </div>
 </template>
 
@@ -11,13 +41,20 @@ import { ref } from "vue";
 
 
   const products = ref([]) ;
-callApi()
+  const isLoading = ref(false) ;
+
+  callApi()
+
   async function callApi() {
+    isLoading.value = true ;
     const res = await fetch("https://dummyjson.com/products");
    
     const data = await res.json();
-    console.log(data);
     
-    products.value = data ;
+    
+    products.value = data.products ;
+    isLoading.value = false ;
+
   }
+
 </script>
