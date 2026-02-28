ed968ea9ffd757a1ff77de2ba15f7133d951a0e7
Mohan Manirul <fiforeg@gmail.com>
2026-02-28 22:59:39 +0600

    return in function

---
+++ laravel/batch12/module2/class/1.php
@@ -0,0 +1,23 @@
+<?php
+
+// function add( $a , $b ){
+//    return $a + $b ;
+// }
+
+// function showResult(){
+//     $sum = add(5,8) ;
+//     echo "Result is : " . $sum ;
+// }
+
+// showResult() ;
+function embedVideo( $ylink , $b ){
+   return $a + $b ;
+}
+
+function showResult(){
+    $sum = add(5,8) ;
+    echo "Result is : " . $sum ;
+}
+
+showResult() ;
+
